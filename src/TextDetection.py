531f4ce084233c2c6dfc28ee5a0250e438081dcd
jade <jadehh@live.com>
2021-09-01 17:26:33 +0800

    update Paddle2.0 TextDetection.py

---
+++ src/TextDetection.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File     : TextDetection.py.py
+# @Author   : jade
+# @Date     : 2021/9/1 17:26
+# @Email    : jadehh@1ive.com
+# @Software : Samples
+# @Desc     :
